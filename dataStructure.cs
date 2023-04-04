eb8596a38671bb676eb7c3aba6005e45599b6119
norbikajtar <norbikajtar@gmail.com>
2023-04-04 10:59:47 +0300

    Create dataStructure.cs

---
+++ dataStructure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CityInfo
+{
+  class datastructure
+  {
+    public readonly DatabaseContext _ctx;
+
+    public datastructure(DatabaseContext ctx)
+    {
+      _ctx = ctx;
+    }
+
+    public void GetCities()
+    {
+      return _ctx.Cities.ToList();
+    }
+
+    public void GetCityDetails(int cityId)
+    {
+      var City = _ctx.Cities.FirstOrDefault(c => c.Id == cityId);
+
+      var Information = GetInformation(City.Id);
+    }
+
+    public void GetCountryDetails(int cityId)
+    {
+      return GetCountryInformation(cityId);
+    }
+
+    public void GetInformation(int cityId)
+    {
+      var city = _ctx.Cities.FirstOrDefault(c => c.Id == cityId);
+
+      return city.Details;
+    }
+
+    public void GetCountryInformation(int cityId)
+    {
+      var city = _ctx.Cities.FirstOrDefault(c => c.Id == cityId);
+
+      return city.Country.Details.Description;
+    }
+
+
+  }
+}
